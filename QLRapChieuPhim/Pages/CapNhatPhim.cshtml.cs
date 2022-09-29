5e140df00427e9de15295910db08172b57c6fb50
hv <hv@PM304-14>
2022-09-29 18:20:25 +0700

    Add project files.

---
+++ QLRapChieuPhim/Pages/CapNhatPhim.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QLRapChieuPhim.Pages
+{
+    public class CapNhatPhimModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();           
+        }
+        public void OnPost()
+        {
+            
+        }
+        
+    }
+}
